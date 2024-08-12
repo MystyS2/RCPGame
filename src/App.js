1b5ba04349f7e35458639570c083c9e88d0f8000
MiHyeon <76768575+MystyS2@users.noreply.github.com>
2024-08-12 21:55:29 +0900

    #1 가위바위보

--- src/App.js
+++ src/App.js
@@ -1,12 +1,87 @@
+import React from "react";
+import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
+// 폰트어썸 import
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandBackFist } from "@fortawesome/free-solid-svg-icons";
+import { faHandScissors } from "@fortawesome/free-solid-svg-icons";
+import { faHand } from "@fortawesome/free-solid-svg-icons";
+
+/*
+[ 요구사항 ]
+ 1. 박스 2개(user, computer)
+ 2. 가위, 바위, 보 버튼
+ 3. 버튼을 클릭하면 해당 값이 user 박스에 나타남
+ 4. computer의 선택은 랜덤하게 정해짐
+ 5. 3, 4의 결과를 비교해 승패를 따짐
+ 6. win-초록, lose-빨강, tie-검정 색으로 박스 테두리 설정
+*/
+
+const choice = {
+  rock:{
+    name:"Rock",
+    img:"https://www.pngfind.com/pngs/b/56-563277_rock-paper-scissors-png-rock-paper-scissors-clipart.png"
+  },
+  scissors:{
+    name:"Scissors",
+    img:"https://www.seekpng.com/png/detail/111-1114370_rock-paper-scissors-rock-paper-scissors-clipart.png"
+
+  },
+  paper:{
+    name:"Paper",
+    img:"https://www.vhv.rs/dpng/d/490-4906131_rock-paper-scissors-clipart-rock-paper-scissors-png.png"
+  },
+};
+
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const play =(userChoice)=>{
+    // user의 선택
+    setUserSelect(choice[userChoice]);
+    // computer의 선택
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    // 선택 결과 비교
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    return choice[itemArray[randomItem]];
+  };
+
+  const judgement =(user, computer) =>{
+    if(user.name === computer.name){
+      return "TIE";
+    } else if(user.name === "Rock")
+      return computer.name === "Scissors" ? "WIN" : "LOSE";
+    else if(user.name === "Scissors")
+      return computer.name === "Paper" ? "WIN" : "LOSE";
+    else if(user.name === "Paper")
+      return computer.name === "Rock" ? "WIN" : "LOSE";
+  };
+
+
   return (
-    <div>
-      <Box name="V"/>
-      <Box name="Mysta"/>
-      <Box name="J1nu"/>
+    <div className="Container">
+      <h1>가위 바위 보!</h1>
+      <div className='Boxes'>
+        <Box title="User" item={userSelect} result = {result}/>
+        <Box title="Computer" item={computerSelect} result = {result}/>
+      </div>
+
+      <div className='Buttons'>
+        <button onClick={() => play("rock")}><FontAwesomeIcon icon={faHandBackFist} size="2x"/></button>
+        <button onClick={() => play("scissors")}><FontAwesomeIcon icon={faHandScissors} size="2x"/></button>
+        <button onClick={() => play("paper")}><FontAwesomeIcon icon={faHand} size="2x"/></button>
+      </div>
+      
     </div>
   );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
 const Box = (props) => {
+  let result;
+
+  if (props.title === "Computer" &&
+    props.result !== "TIE" &&
+    props.result !== null
+  ){
+    result = props.result === "WIN" ? "LOSE" : "WIN";
+  }
+  else{
+    result = props.result;
+  }
+
   return (
-    <div className='box'>
-        Box1
-        <p>{props.name}</p>
+    <div className={`box ${result}`}>
+      <h2>{props.title}</h2>
+      <img className="item-img" src={props.item ? props.item.img : "https://mblogthumb-phinf.pstatic.net/20120711_299/king940_1342016154926eKCVn_JPEG/%C1%D9%B8%AE%BE%F0.jpg?type=w800"} />
+      <h2>{result ? result : "게임을 시작해주세요!"}</h2>
     </div>
   )
 }
