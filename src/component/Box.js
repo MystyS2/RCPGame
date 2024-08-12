ab9c3e213db978845b44fd5e71722bbc2c2b4311
MiHyeon <76768575+MystyS2@users.noreply.github.com>
2024-08-12 22:19:22 +0900

    #1 배포 시도

--- src/component/Box.js
+++ src/component/Box.js
@@ -16,7 +16,7 @@ const Box = (props) => {
   return (
     <div className={`box ${result}`}>
       <h2>{props.title}</h2>
-      <img className="item-img" src={props.item ? props.item.img : "https://mblogthumb-phinf.pstatic.net/20120711_299/king940_1342016154926eKCVn_JPEG/%C1%D9%B8%AE%BE%F0.jpg?type=w800"} />
+      <img className="item-img" alt="" src={props.item ? props.item.img : "https://mblogthumb-phinf.pstatic.net/20120711_299/king940_1342016154926eKCVn_JPEG/%C1%D9%B8%AE%BE%F0.jpg?type=w800"} />
       <h2>{result ? result : "게임을 시작해주세요!"}</h2>
     </div>
   )
